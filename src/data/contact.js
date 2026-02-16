4a956a1435b3645acd56ff20d740b40c627093ab
taiyyoson <tpwilliamson@dons.usfca.edu>
2026-02-15 18:16:29 -0800

    Add virtual filesystem builder for terminal navigation

---
+++ src/data/contact.js
@@ -0,0 +1,9 @@
+export const CONTACT_TEXT = `Contact
+=======
+
+GitHub:   https://github.com/taiyyoson
+LinkedIn: https://linkedin.com/in/taiyo-williamson
+Email:    taiyo@dons.usfca.edu
+
+Use "open <url>" to open any link in a new tab.
+`;
